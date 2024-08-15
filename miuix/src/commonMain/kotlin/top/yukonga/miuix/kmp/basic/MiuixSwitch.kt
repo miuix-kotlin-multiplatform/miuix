b5daf90dbcdd7451452948643cc3dafbe8e503d5
YuKongA <70465933+YuKongA@users.noreply.github.com>
2024-08-15 12:28:24 +0800

    library: Add Switch drag support

--- miuix/src/commonMain/kotlin/top/yukonga/miuix/kmp/basic/MiuixSwitch.kt
+++ miuix/src/commonMain/kotlin/top/yukonga/miuix/kmp/basic/MiuixSwitch.kt
@@ -6,6 +6,8 @@ import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.animation.core.spring
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.detectDragGestures
+import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.requiredSize
@@ -16,18 +18,22 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.minimumInteractiveComponentSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberUpdatedState
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.hapticfeedback.HapticFeedbackType
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.platform.LocalHapticFeedback
 import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import top.yukonga.miuix.kmp.theme.MiuixTheme
+import kotlin.math.absoluteValue
 
 @Composable
 fun MiuixSwitch(
@@ -37,29 +43,43 @@ fun MiuixSwitch(
     enabled: Boolean = true,
     interactionSource: MutableInteractionSource? = remember { MutableInteractionSource() },
 ) {
+    val isChecked by rememberUpdatedState(checked)
     val hapticFeedback = LocalHapticFeedback.current
+    var hasVibrated by remember { mutableStateOf(false) }
     val springSpec = remember {
         spring<Dp>(
             dampingRatio = Spring.DampingRatioLowBouncy,
             stiffness = Spring.StiffnessMedium
         )
     }
+    var isPressed by remember { mutableStateOf(false) }
+    var dragOffset by remember { mutableStateOf(0f) }
     val thumbOffset by animateDpAsState(
-        targetValue = if (checked) 28.dp else 4.dp,
+        targetValue = if (isChecked) {
+            if (isPressed) 26.5.dp else 28.dp
+        } else {
+            if (isPressed) 2.5.dp else 4.dp
+        } + dragOffset.dp,
         animationSpec = springSpec
     )
+
+    val thumbSize by animateDpAsState(
+        targetValue = if (isPressed) 23.dp else 20.dp,
+        animationSpec = springSpec
+    )
+
     val backgroundColor by animateColorAsState(
-        if (checked) MiuixTheme.colorScheme.primary else MiuixTheme.colorScheme.switchThumb,
+        if (isChecked) MiuixTheme.colorScheme.primary else MiuixTheme.colorScheme.switchThumb,
         animationSpec = tween(durationMillis = 200)
     )
     val disabledBackgroundColor by rememberUpdatedState(
-        if (checked) MiuixTheme.colorScheme.disabledBg else MiuixTheme.colorScheme.primaryContainer
+        if (isChecked) MiuixTheme.colorScheme.disabledBg else MiuixTheme.colorScheme.primaryContainer
     )
-    val toggleableModifier = remember(onCheckedChange, checked, enabled) {
+    val toggleableModifier = remember(onCheckedChange, isChecked, enabled) {
         if (onCheckedChange != null) {
             Modifier.minimumInteractiveComponentSize()
                 .toggleable(
-                    value = checked,
+                    value = isChecked,
                     onValueChange = {
                         onCheckedChange(it)
                         hapticFeedback.performHapticFeedback(HapticFeedbackType.LongPress)
@@ -81,12 +101,67 @@ fun MiuixSwitch(
             .requiredSize(52.dp, 28.5.dp)
             .clip(RoundedCornerShape(50.dp))
             .background(if (enabled) backgroundColor else disabledBackgroundColor)
+            .pointerInput(Unit) {
+                detectTapGestures(
+                    onPress = {
+                        isPressed = true
+                    },
+                    onTap = {
+                        isPressed = false
+                        if (enabled) {
+                            hapticFeedback.performHapticFeedback(HapticFeedbackType.LongPress)
+                            onCheckedChange?.invoke(!isChecked)
+                        }
+                    }
+                )
+            }
+            .pointerInput(Unit) {
+                detectDragGestures(
+                    onDragStart = {
+                        isPressed = true
+                        hasVibrated = false
+                    },
+                    onDragEnd = {
+                        isPressed = false
+                        val switchWidth = 24f
+                        if (dragOffset.absoluteValue > switchWidth / 2) {
+                            if (enabled) onCheckedChange?.invoke(!isChecked)
+                        }
+                        dragOffset = 0f
+                    },
+                    onDragCancel = {
+                        isPressed = false
+                        dragOffset = 0f
+                    },
+                    onDrag = { change, dragAmount ->
+                        if (!enabled) return@detectDragGestures
+                        val newOffset = dragOffset + dragAmount.x / 2
+                        dragOffset = if (isChecked) newOffset.coerceIn(-24f, 0f) else newOffset.coerceIn(0f, 24f)
+                        if (isChecked) {
+                            if (dragOffset in -23f..-1f) {
+                                hasVibrated = false
+                            } else if ((dragOffset == -24f || dragOffset == 0f) && !hasVibrated) {
+                                hapticFeedback.performHapticFeedback(HapticFeedbackType.LongPress)
+                                hasVibrated = true
+                            }
+                        } else {
+                            if (dragOffset in 1f..23f) {
+                                hasVibrated = false
+                            } else if ((dragOffset == 0f || dragOffset == 24f) && !hasVibrated) {
+                                hapticFeedback.performHapticFeedback(HapticFeedbackType.LongPress)
+                                hasVibrated = true
+                            }
+                        }
+                        change.consume()
+                    }
+                )
+            }
     ) {
         MiuixBox(
             modifier = Modifier
                 .padding(start = thumbOffset)
                 .align(Alignment.CenterStart)
-                .size(20.dp)
+                .size(thumbSize)
                 .background(Color.White, shape = RoundedCornerShape(50.dp))
         )
     }
