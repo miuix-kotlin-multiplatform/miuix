4c00307c4a9549dd6d519829b23a92ec441b780a
余空 <70465933+YuKongA@users.noreply.github.com>
2024-08-28 14:53:43 +0800

    ci: Push example App to CI channel

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -5,6 +5,7 @@ import org.jetbrains.compose.desktop.application.dsl.TargetFormat
 import org.jetbrains.kotlin.gradle.ExperimentalWasmDsl
 import org.jetbrains.kotlin.gradle.plugin.mpp.apple.XCFramework
 import org.jetbrains.kotlin.gradle.targets.js.webpack.KotlinWebpackConfig
+import java.util.Properties
 
 plugins {
     alias(libs.plugins.android.application)
@@ -88,6 +89,31 @@ android {
         versionCode = 1
         versionName = verName
     }
+    val properties = Properties()
+    runCatching { properties.load(project.rootProject.file("local.properties").inputStream()) }
+    val keystorePath = properties.getProperty("KEYSTORE_PATH") ?: System.getenv("KEYSTORE_PATH")
+    val keystorePwd = properties.getProperty("KEYSTORE_PASS") ?: System.getenv("KEYSTORE_PASS")
+    val alias = properties.getProperty("KEY_ALIAS") ?: System.getenv("KEY_ALIAS")
+    val pwd = properties.getProperty("KEY_PASSWORD") ?: System.getenv("KEY_PASSWORD")
+    if (keystorePath != null) {
+        signingConfigs {
+            register("github") {
+                storeFile = file(keystorePath)
+                storePassword = keystorePwd
+                keyAlias = alias
+                keyPassword = pwd
+                enableV3Signing = true
+                enableV4Signing = true
+            }
+        }
+    } else {
+        signingConfigs {
+            register("release") {
+                enableV3Signing = true
+                enableV4Signing = true
+            }
+        }
+    }
     dependenciesInfo.includeInApk = false
     packaging {
         applicationVariants.all {
@@ -98,14 +124,15 @@ android {
         resources.excludes += "**"
     }
     buildTypes {
-        getByName("release") {
+        release {
             isMinifyEnabled = true
             isShrinkResources = true
             vcsInfo.include = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+            signingConfig = signingConfigs.getByName(if (keystorePath != null) "github" else "release")
+        }
+        debug {
+            if (keystorePath != null) signingConfig = signingConfigs.getByName("github")
         }
     }
 
