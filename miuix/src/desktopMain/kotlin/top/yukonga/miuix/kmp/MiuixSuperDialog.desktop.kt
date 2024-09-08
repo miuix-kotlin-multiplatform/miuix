27cfb0e1334528cbab48191fb04e4abefc2118b9
余空 <70465933+YuKongA@users.noreply.github.com>
2024-09-08 10:50:59 +0800

    library: desktop: Add JVM platform BackHandler support

--- miuix/src/desktopMain/kotlin/top/yukonga/miuix/kmp/MiuixSuperDialog.desktop.kt
+++ miuix/src/desktopMain/kotlin/top/yukonga/miuix/kmp/MiuixSuperDialog.desktop.kt
@@ -3,6 +3,8 @@ package top.yukonga.miuix.kmp
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import java.awt.KeyboardFocusManager
+import java.awt.event.KeyEvent
 
 @Composable
 actual fun getRoundedCorner(): Dp = 0.dp
@@ -12,4 +14,11 @@ actual fun BackHandler(
     dismiss: () -> Unit,
     onDismissRequest: () -> Unit
 ) {
+    KeyboardFocusManager.getCurrentKeyboardFocusManager().addKeyEventDispatcher { event ->
+        if (event.keyCode == KeyEvent.VK_ESCAPE) {
+            dismiss()
+            onDismissRequest()
+        }
+        false
+    }
 }
