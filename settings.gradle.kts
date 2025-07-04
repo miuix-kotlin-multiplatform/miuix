d6a03fd47956a89965112db72425fd30572e9690
YuKongA <70465933+YuKongA@users.noreply.github.com>
2025-07-04 14:55:21 +0800

    build(deps): bump AGP from 8.10.1 to 8.11.0

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -32,8 +32,8 @@ dependencyResolutionManagement {
 }
 
 plugins {
-    id("com.android.settings") version ("8.10.1")
-    id("org.gradle.toolchains.foojay-resolver-convention") version ("0.9.0")
+    id("com.android.settings") version ("8.11.0")
+    id("org.gradle.toolchains.foojay-resolver-convention") version ("1.0.0")
 }
 
 android {
