a8f33f615a37e1f621cf753380e25b9f6527f361
YuKongA <70465933+YuKongA@users.noreply.github.com>
2025-07-31 21:09:47 +0800

    library: Refactor Icon from androidx-main

--- miuix/src/commonMain/kotlin/top/yukonga/miuix/kmp/basic/Icon.kt
+++ miuix/src/commonMain/kotlin/top/yukonga/miuix/kmp/basic/Icon.kt
@@ -8,11 +8,14 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawWithCache
 import androidx.compose.ui.draw.paint
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.graphics.ColorProducer
 import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.graphics.layer.drawLayer
 import androidx.compose.ui.graphics.painter.BitmapPainter
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.graphics.toolingGraphicsLayer
@@ -25,6 +28,7 @@ import androidx.compose.ui.semantics.role
 import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.unit.dp
 import org.jetbrains.compose.resources.stringResource
+import top.yukonga.miuix.kmp.theme.LocalContentColor
 
 /**
  * A [Icon] component that draws [imageVector] using [tint], with a default value.
@@ -43,7 +47,7 @@ fun Icon(
     imageVector: ImageVector,
     contentDescription: String?,
     modifier: Modifier = Modifier,
-    tint: Color = IconDefaults.defaultTint()
+    tint: Color = LocalContentColor.current,
 ) {
     Icon(
         painter = rememberVectorPainter(imageVector),
@@ -70,7 +74,7 @@ fun Icon(
     bitmap: ImageBitmap,
     contentDescription: String?,
     modifier: Modifier = Modifier,
-    tint: Color = IconDefaults.defaultTint()
+    tint: Color = LocalContentColor.current,
 ) {
     val painter = remember(bitmap) { BitmapPainter(bitmap) }
     Icon(
@@ -98,43 +102,73 @@ fun Icon(
     painter: Painter,
     contentDescription: String?,
     modifier: Modifier = Modifier,
-    tint: Color = IconDefaults.defaultTint()
+    tint: Color = LocalContentColor.current,
 ) {
-    val colorFilter = if (tint == Color.Unspecified) null else ColorFilter.tint(tint)
-
+    val colorFilter =
+        remember(tint) { if (tint == Color.Unspecified) null else ColorFilter.tint(tint) }
+    val semantics =
+        if (contentDescription != null) {
+            Modifier.semantics {
+                this.contentDescription = contentDescription
+                this.role = Role.Image
+            }
+        } else {
+            Modifier
+        }
     Box(
-        modifier = modifier
+        modifier
             .toolingGraphicsLayer()
-            .then(
-                if (painter.intrinsicSize == Size.Unspecified || painter.intrinsicSize.isInfinite()) {
-                    DefaultIconSizeModifier
-                } else {
-                    Modifier
-                }
-            )
+            .defaultSizeFor(painter)
             .paint(painter, colorFilter = colorFilter, contentScale = ContentScale.Fit)
-            .then(
-                if (contentDescription != null) {
-                    Modifier.semantics {
-                        this.contentDescription = contentDescription
-                        this.role = Role.Image
-                    }
-                } else {
-                    Modifier
-                }
-            )
+            .then(semantics)
     )
 }
+
+/**
+ * A [Icon] component that draws [painter] using [tint], with a default value.
+ *
+ * @param painter [Painter] to draw inside this icon
+ * @param tint tint to be applied to [painter]. If null, then no tint is applied.
+ * @param contentDescription text used by accessibility services to describe what this icon
+ *   represents. This should always be provided unless this icon is used for decorative purposes,
+ *   and does not represent a meaningful action that a user can take. This text should be localized,
+ *   such as by using [stringResource] or similar
+ * @param modifier the [Modifier] to be applied to this icon
+ */
+@Composable
+fun Icon(
+    painter: Painter,
+    tint: ColorProducer?,
+    contentDescription: String?,
+    modifier: Modifier = Modifier,
+) {
+    Icon(
+        painter = painter,
+        tint = Color.Unspecified,
+        contentDescription = contentDescription,
+        modifier =
+            modifier.drawWithCache {
+                val layer = obtainGraphicsLayer()
+                layer.apply {
+                    record { drawContent() }
+                    tint?.let { this@apply.colorFilter = ColorFilter.tint(it()) }
+                }
+                onDrawWithContent { drawLayer(graphicsLayer = layer) }
+            },
+    )
+}
+
+private fun Modifier.defaultSizeFor(painter: Painter) =
+    this.then(
+        if (painter.intrinsicSize == Size.Unspecified || painter.intrinsicSize.isInfinite()) {
+            DefaultIconSizeModifier
+        } else {
+            Modifier
+        }
+    )
+
 
 private fun Size.isInfinite() = width.isInfinite() && height.isInfinite()
 
 // Default icon size, for icons with no intrinsic size information
 private val DefaultIconSizeModifier = Modifier.size(24.dp)
-
-object IconDefaults {
-    /**
-     * The default tint of the [Icon].
-     */
-    @Composable
-    fun defaultTint() = Color.Unspecified
-}
